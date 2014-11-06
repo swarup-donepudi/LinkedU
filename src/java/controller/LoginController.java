09bcef1a0efba335bc8e6fedf9eb4638108002ac
Swarup Donepudi <swarup.donepudi@gmail.com>
2014-11-06 14:24:14 -0600

    Added the Login Controller

---
+++ src/java/controller/LoginController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import model.LoginBean;
+
+/**
+ *
+ * @author skdonep
+ */
+@Named(value = "loginController")
+@SessionScoped
+public class LoginController implements Serializable {
+    
+    LoginBean loginBean;
+
+    /**
+     * Creates a new instance of LoginController
+     */
+    public LoginController() {
+        loginBean = new LoginBean();
+    }
+
+    public LoginBean getLoginBean() {
+        return loginBean;
+    }
+
+    public void setLoginBean(LoginBean loginBean) {
+        this.loginBean = loginBean;
+    }
+    
+    public void validateCredentials(){
+    
+    }
+    
+}
---
+++ src/java/model/LoginBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author skdonep
+ */
+public class LoginBean {
+    private String userName;
+    private String password;
+    
+    public LoginBean(){
+        
+    }    
+    public LoginBean(String userName,String password){
+        this.userName=userName;
+        this.password=password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }         
+}
