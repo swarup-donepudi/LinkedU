afbb73a8df84ce80ca382a3900c50a8a34e4a1e3
Swarup Donepudi <swarup.donepudi@gmail.com>
2014-11-18 12:02:58 -0600

    Added JSF templates to the app for Header, Content and Footer.
    Also, I created NavingationController. Going forward we shall this
    controller to handle all the logic related to the navigation.

---
+++ src/java/controller/NavigationController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+/**
+ *
+ * @author skdonep
+ */
+@ManagedBean(name = "navigationController")
+@SessionScoped
+public class NavigationController implements Serializable {
+    
+    @ManagedProperty(value = "#{loginController}")
+    private LoginController loginController;
+
+    /**
+     * Creates a new instance of NavigationController
+     */
+    public NavigationController() {
+    }
+
+    public LoginController getLoginController() {
+        return loginController;
+    }
+
+    public void setLoginController(LoginController loginController) {
+        this.loginController = loginController;
+    }
+    
+    public String redirectToUserHomepage(){
+        if(loginController.loginBean.getAccountType()=='S')
+            return ("StudentHome.xhtml");
+        else
+            return ("RecruiterHome.xhtml");
+    }
+}
