42286aaf204f0d2679afc1682321dc4acbfaf1c1
Swarup Donepudi <swarup.donepudi@gmail.com>
2014-11-19 11:45:51 -0600

    Revert "Revert "Revert "Up"""
    This reverts commit 2df6778524e5465b4e16adbd7fb01f81a3b4eb78.

--- src/java/controller/SignupController.java
+++ src/java/controller/SignupController.java
@@ -1,5 +1,5 @@
 /*
- * To change tthis license header, choose License Headers in Project Properties.
+ * To changee this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
