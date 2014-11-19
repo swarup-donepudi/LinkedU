b65cbda4841acd7f4e0cfc8c1a3cdfad1764e877
Swarup Donepudi <swarup.donepudi@gmail.com>
2014-11-19 11:42:49 -0600

    Up

--- src/java/controller/SignupController.java
+++ src/java/controller/SignupController.java
@@ -1,5 +1,5 @@
 /*
- * To changee this license header, choose License Headers in Project Properties.
+ * To change tthis license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
